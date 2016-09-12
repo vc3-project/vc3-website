1a450e608ca6b8d7cbadec66431225e474608d2f
John Jung <john@fieldrotation.com>
2016-09-12 06:24:10 -0500

    Rebuilt site.

--- javascripts/vcc.js
+++ javascripts/vcc.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $('.default-slider').unslider({
     arrows: false,
     autoplay: true,
-    dots: true
+    delay: 8000,
+    dots: true,
+    speed: 1000
   });
 });
